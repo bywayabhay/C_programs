5f81ae2fae4357784a47f537a7c9f4a050da0d40
bywayabhay <singhabhay2085@gmail.com>
2024-10-09 21:49:57 +0530

    compound interest corrected

--- lab01exp_02.c
+++ lab01exp_02.c
@@ -1,21 +1,25 @@
 // 2. WAP that calculate the simple interest and compound interest. The principal amount, rate of interest and time are entered through the keyboard
 #include <stdio.h>
+#include <math.h>
 int main()
 {
-    float simpleInterest, amount, interestRate, time;
+    float simpleInterest, compoundInterrest, principal, amount, interestRate, time;
 
-    //taking user input
+    // taking user input
     printf("Enter amount:");
-    scanf("%f", &amount);
+    scanf("%f", &principal);
     printf("Enter interest rate (in percentage):");
     scanf("%f", &interestRate);
     printf("Enter time in years:");
     scanf("%f", &time);
 
-    //calculating SI
-    simpleInterest = (amount * interestRate * time) / 100;
+    // calculating SI and CI
+    simpleInterest = (principal * interestRate * time) / 100;
+    amount = principal * ((pow((1 + interestRate / 100), time)));
+    compoundInterrest = amount - principal;
 
-    //printing SI
-    printf("Simple interest is: %f after %f years", simpleInterest, time);
+    // printing SI
+    printf("Simple interest is: %f after %f years\n", simpleInterest, time);
+    printf("Compound interest is: %f after %f years", compoundInterrest, time);
     return 0;
 }
